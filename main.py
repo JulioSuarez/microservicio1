50278ed87b8dee26bf099c10620c8d0918ce2a89
Julio Suarez <julio.suarez91@gmail.com>
2023-11-19 15:22:59 -0400

    prueba3

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.include_router(products.router)
+app.include_router(users.router)
+
+app.include_router(basic_auth_users.router)
+
+app.include_router(jwt_auth_users.router)
+
+app.include_router(users_db.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World prueba 2"}
+    return "Hola FastAPI!"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/url")
+async def url():
+    return {"url": "https://mouredev.com/python"}

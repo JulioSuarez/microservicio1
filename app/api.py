766063a7709d1f6b06140cd75de67a3671c8e867
Julio Suarez <julio.suarez91@gmail.com>
2023-11-19 18:52:12 -0400

    nuevo

---
+++ app/api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.include_router(products.router)
+app.include_router(users.router)
+
+app.include_router(basic_auth_users.router)
+
+app.include_router(jwt_auth_users.router)
+
+app.include_router(users_db.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
+
+@app.get("/url")
+async def url():
+    return {"url": "https://mouredev.com/python"}
+
+# token para delta space
+# B5anJF9V_fhA8ZUFDbB11c9EbdEsMW5rrNQXnUsJL
--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-from fastapi import FastAPI
-from routers import products, users, basic_auth_users, jwt_auth_users, users_db
-from fastapi.staticfiles import StaticFiles
+import uvicorn
+from os import getenv
 
-app = FastAPI()
-
-app.include_router(products.router)
-app.include_router(users.router)
-
-app.include_router(basic_auth_users.router)
-
-app.include_router(jwt_auth_users.router)
-
-app.include_router(users_db.router)
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/")
-async def root():
-    return "Hola FastAPI!"
-
-@app.get("/url")
-async def url():
-    return {"url": "https://mouredev.com/python"}
-
-# token para delta space
-# B5anJF9V_fhA8ZUFDbB11c9EbdEsMW5rrNQXnUsJL
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("app.api:app",host="0.0.0.0", port=port, reload=True)
